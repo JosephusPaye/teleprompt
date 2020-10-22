8afea6ac3a752e173e1fbc2678e28bf4362427a0
Josephus Paye II <j.paye96@gmail.com>
2020-10-22 04:26:12 +0000

    Add favicon and iOS icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,9 +2,31 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui">
     <title>Teleprompt</title>
-    <link rel="icon" id="emojicon" href="/favicon.ico" />
+    <meta
+        name="description"
+        content="A fast and simple teleprompter you can control with a presentation remote."
+    />
+    <meta
+        name="og:description"
+        content="A fast and simple teleprompter you can control with a presentation remote."
+    />
+    <meta name="og:url" content="https://teleprompt.webpluslabs.com/" />
+    <meta name="og:title" content="Teleprompt" />
+    <link
+        id="emojicon"
+        rel="shortcut icon"
+        href="/favicon.ico"
+    />
+    <link
+        rel="apple-touch-icon"
+        sizes="194x194"
+        href="/apple-touch-icon.png"
+        type="image/png"
+    />
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <script>
         window.backendData = @json($backendData);
     </script>
